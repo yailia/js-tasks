27a74b712cc2ee389c150305483f505163caad7d
Ivanov I <ilia@ivanov.press>
2021-02-03 21:31:57 +0300

    test of bigger array

--- spiral.js
+++ spiral.js
@@ -1,52 +1,54 @@
 'use strict'
 const array = [
-[1,  2,  3,  4,  5],
-[6,  7,  8,  9,  10],
-[11, 12, 13, 14, 15],
-[16, 17, 18, 19, 20]
+[1,  2,  3,  4,  5, 6, 7],
+[8,  9,  10,  11,  12, 13, 14],
+[15, 16, 17, 18, 19, 20, 21],
+[22, 23, 24, 25, 26, 27, 28],
+[29, 30, 31, 32, 33, 34, 35],
+[36, 37, 38, 39, 40, 41, 42]
 ];
 
 
 function goRight (arr) {
   if(array){
-    for (let i of arr[0]) console.log(i)
-    arr.shift()
+    for (let i of arr[0]) console.log(i);
+    arr.shift();
   }
-  return arr
+  return arr;
 };
 
 function goDown (arr) {
   if(array) {
-    for (let i of arr) console.log(i.pop())
+    for (let i of arr) console.log(i.pop());
   } 
-  return arr
+  return arr;
 };
 
 function goLeft (arr) {
   if (array){
-    const lastI = arr.length - 1
+    const lastI = arr.length - 1;
     const needArr = arr[lastI];
     for (let i = lastI + 1; i >= 0; i--) console.log(needArr[i])
-    arr.pop()
+    arr.pop();
   }
-  return arr
+  return arr;
 };
 
 function goUp (arr) {
   if (array) {
     for (let i = arr.length -1 ; i >=0; i--) console.log(arr[i].shift())
   }
-  return arr
+  return arr;
 };
 
 function loop (array) {
   while(array.length > 0){
-    const right = goRight(array)
-    const down = goDown(right)
-    const left = goLeft(down)
-    const up = goUp(left)
+    const right = goRight(array);
+    const down = goDown(right);
+    const left = goLeft(down);
+    const up = goUp(left);
     }
-    console.log ('Конец')
+    console.log ('Конец');
   };
 
-loop(array)
+loop(array);
