5b10716542b769ea27227815a99f92fa33930275
Ivanov I <ilia@ivanov.press>
2021-02-24 21:54:05 +0300

    +

---
+++ promise.js
@@ -0,0 +1,9 @@
+// Напишите функцию delay(ms), которая возвращает промис, 
+// переходящий в состояние "resolved" через ms миллисекунд.
+
+
+function delay(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
+}
